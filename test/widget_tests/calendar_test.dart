232bbe35364eb016226f6b2270bece3630938a1f
mickael-h <mickael.hassine@gmail.com>
2020-11-10 13:42:19 +0100

    tests

--- test/widget_tests/calendar_test.dart
+++ test/widget_tests/calendar_test.dart
@@ -9,14 +9,44 @@ void main() {
     await initializeDateFormatting();
 
     final bindings = MockPlanningBindings();
-    when(bindings.functionTools
-            .callFunction('getPlanning', data: anyNamed('data')))
-        .thenAnswer((_) => Future.sync(() => planningsExample));
-    final calendar = find.byKey(ValueKey('calendar'));
+    when(bindings.functionTools.callFunction(
+      'getPlanning',
+      data: {'date': '2020-08-20'},
+    )).thenAnswer((_) => Future.sync(() => planningsExample));
 
     await tester.pumpWidget(GetMaterialApp(
       home: PlanningScreen(),
       initialBinding: bindings,
     ));
+
+    final calendarLeftFinder = find.byKey(ValueKey('calendarButtonLeft'));
+    expect(calendarLeftFinder, findsOneWidget);
+
+    final calendarRightFinder = find.byKey(ValueKey('calendarButtonRight'));
+    expect(calendarRightFinder, findsOneWidget);
+
+    while (find.text('août 2020').evaluate().isEmpty &&
+        find.text('20').evaluate().isEmpty) {
+      await tester.tap(calendarLeftFinder);
+      await tester.pump();
+    }
+
+    final calendarDay1Finder = find.byKey(ValueKey('calendarButtonDay1'));
+    expect(calendarDay1Finder, findsOneWidget);
+    await tester.tap(calendarDay1Finder);
+    await tester.pump();
+    expect(find.byType(PlanningListEntry), findsNothing);
+
+    final calendarDay4Finder = find.byKey(ValueKey('calendarButtonDay4'));
+    expect(calendarDay4Finder, findsOneWidget);
+    await tester.tap(calendarDay4Finder);
+    await tester.pump();
+    expect(find.byType(PlanningListEntry), findsOneWidget);
+    expect(
+      find.text('119, Express Bourg-en-Bresse -> Villefranche-sur-Saône 6:45'),
+      findsOneWidget,
+    );
+
+    print('passed!');
   });
 }
